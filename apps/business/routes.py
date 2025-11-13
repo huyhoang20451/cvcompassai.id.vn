71cd1cc739012f77fb6bd0d1ade81dab28bcf81b
khoa150 <dkhoa17022004@gmail.com>
2025-11-13 11:45:43 +0700

    update

--- apps/business/routes.py
+++ apps/business/routes.py
@@ -179,22 +179,22 @@ def compare_cv_vs_jd_start(request: Request,
     html = f"""
     <html>
     <style>
-        .progress-container {
-        text-align: center;
-        display: flex;
-        margin-top: 100px;
-        flex-direction: column;
-        align-items: center;
-    }
-    .progress-container h3 {
-        margin-bottom: 20px;
-        font-size: 24px;
-        color: #333;
-    }
-    .progress-container progress {
-        width: 300px;
-        height: 25px;
-    }
+        .progress-container {{
+            text-align: center;
+            display: flex;
+            margin-top: 100px;
+            flex-direction: column;
+            align-items: center;
+        }}
+        .progress-container h3 {{
+            margin-bottom: 20px;
+            font-size: 24px;
+            color: #333;
+        }}
+        .progress-container progress {{
+            width: 300px;
+            height: 25px;
+        }}
     </style>
     <body>
       <div class="progress-container">
